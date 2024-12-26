7891d489ecc363d86f071e6bcec8a1f6134c2051
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-12-26 23:15:55 +0100

    Add files via upload

---
+++ src/main/java/com/resellerapp/service/ConditionService.java
@@ -0,0 +1,30 @@
+package com.resellerapp.service;
+
+import com.resellerapp.model.entity.Condition;
+import com.resellerapp.model.enums.ConditionType;
+import com.resellerapp.repository.ConditionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+@Service
+public class ConditionService {
+    private final ConditionRepository conditionRepository;
+
+    @Autowired
+    public ConditionService(ConditionRepository conditionRepository) {
+        this.conditionRepository = conditionRepository;
+    }
+
+    @PostConstruct
+    private void postConstruct() {
+        if (this.conditionRepository.count() == 0) {
+            this.conditionRepository.saveAllAndFlush(Arrays.stream(ConditionType.values())
+                    .map(Condition::new)
+                    .collect(Collectors.toList()));
+        }
+    }
+}
